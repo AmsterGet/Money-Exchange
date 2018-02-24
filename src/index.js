21fd8eafa4f021fe03f4f84bdeb21f13ecb76da4
AmsterGet <amsterget@gmail.com>
2018-02-24 19:28:08 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    if (currency<=0) {
+        return {};
+    }
+    if (currency>=10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    const currencyConfig = {"H":50,"Q":25,"D":10,"N":5,"P":1};
+    let result = {};
+    let currencyValue = currency;
+    for (currencyType in currencyConfig) {
+        if(currencyValue===0){
+            break;
+        }
+
+        let currencyNominal = currencyConfig[currencyType];
+        let currentValue=Math.trunc(currencyValue/currencyNominal);
+        if(!currentValue){
+            continue;
+        }
+        result[currencyType]=currentValue;
+        currencyValue%=currencyNominal;
+    }
+    return result;
+};
